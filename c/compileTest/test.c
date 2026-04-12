bc7e525d19b740918de9c169b36ae7a6fb33ac20
—͟͞͞★ ɪᴋ𝘂ᵞο ᴋ𝖎𝘵𝕒 ★ <ayumi.aiko@outlook.com>
2026-04-12 13:07:35 +0000

    add: Added license template and notice in the code

--- c/compileTest/test.c
+++ c/compileTest/test.c
@@ -1,3 +1,19 @@
+//
+// Copyright (C) 2025 —͟͞͞★ ɪᴋ𝘂ᵞο ᴋ𝖎𝘵𝕒 ★ <ayumi.aiko@outlook.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 #include <tsukika.h>
 #include <tsukika_props.h>
 char *batteryPercentageBlobFilePaths[] = {NULL};
@@ -10,6 +26,5 @@ char **__propertiesValue_cached = NULL;
 int main(void) {
     printf("TARGET_BUILD_SETUP_WIZARD_OUTRO_TEXT: %s\n", getpropFromFile("TARGET_BUILD_SETUP_WIZARD_OUTRO_TEXT", "./src/makeconfigs.prop"));
     prepareStockRecoveryCommandFile(SWITCH_LOCALE, "en", "mx");
-    printf("the pidof mysqld: %d\n", getPidOf("mysqld"));
     return 0;
 }
